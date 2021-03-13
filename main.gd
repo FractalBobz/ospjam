275a0f91285cb5364fb0c9159ec3327a6102fd52
M. Gardner <magardner2010@gmail.com>
2021-03-13 17:22:52 +0200

    add spike cannon to main scene. Also, add physics to a few other things.

--- main.gd
+++ main.gd
@@ -1,6 +1,8 @@
 extends Node
 
-export (PackedScene) var spike;
+export (PackedScene) var Spike;
+export (int) var min_spike_velo;
+export (int) var max_spike_velo;
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -15,3 +17,14 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Timer_timeout():
+	var spike=Spike.instance()
+	add_child(spike)
+	var direction=-3*PI/8
+	spike.position=$Node2D.position
+	spike.rotation=direction
+	spike.linear_velocity=Vector2(rand_range(min_spike_velo,max_spike_velo),0)
+	spike.linear_velocity=spike.linear_velocity.rotated(direction)
+	pass # Replace with function body.
