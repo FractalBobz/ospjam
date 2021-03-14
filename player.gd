6a72b8d3b4c8a0c3879a8050d6302d4f459f71f7
M. Gardner <magardner2010@gmail.com>
2021-03-14 12:08:08 +0200

    add particles

--- player.gd
+++ player.gd
@@ -8,7 +8,11 @@ func _ready():
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_up"):
 		linear_velocity.y=-100
-	pass
+	#if linear_velocity.y==0:
+	#	$Particles2D.emitting=true;
+	#else:
+	#	$Particles2D.emitting=false;
+	#pass
 
 
 func _on_VisibilityNotifier2D_screen_exited():
